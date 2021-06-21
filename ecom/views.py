1dfdd25baea3de5cea2f819fa4029caa377a838d
kirthikask <kirthi21290@gmail.com>
2021-06-22 00:08:48 +0530

    e.commerce
    online shopping

---
+++ ecom/views.py
@@ -0,0 +1,153 @@
+from django.shortcuts import render,redirect,reverse
+from . import forms,models
+from django.http import HttpResponseRedirect,HttpResponse
+
+from django.conf import settings
+
+def home_view(request):
+    products=models.Product.objects.all()
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        counter=product_ids.split('|')
+        product_count_in_cart=len(set(counter))
+    else:
+        product_count_in_cart=0
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return render(request,'ecom/index.html',{'products':products,'product_count_in_cart':product_count_in_cart})
+
+
+#for showing login button for admin(by sumit)
+def adminclick_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('afterlogin')
+    return HttpResponseRedirect('adminlogin')
+
+
+def afterlogin_view(request):
+    if is_customer(request.user):
+        return redirect('customer-home')
+    else:
+        return redirect('admin-dashboard')
+
+@login_required(login_url='adminlogin')
+def admin_dashboard_view(request):
+    # for cards on dashboard
+    customercount=models.Customer.objects.all().count()
+    productcount=models.Product.objects.all().count()
+    ordercount=models.Orders.objects.all().count()
+
+    # for recent order tables
+    orders=models.Orders.objects.all()
+    ordered_products=[]
+    ordered_bys=[]
+    for order in orders:
+        ordered_product=models.Product.objects.all().filter(id=order.product.id)
+        ordered_by=models.Customer.objects.all().filter(id = order.customer.id)
+        ordered_products.append(ordered_product)
+        ordered_bys.append(ordered_by)
+
+    mydict={
+    'customercount':customercount,
+    'productcount':productcount,
+    'ordercount':ordercount,
+    'data':zip(ordered_products,ordered_bys,orders),
+    }
+    return render(request,'ecom/admin_dashboard.html',context=mydict)
+
+
+# admin view customer table
+@login_required(login_url='adminlogin')
+def view_customer_view(request):
+    customers=models.Customer.objects.all()
+    return render(request,'ecom/view_customer.html',{'customers':customers})
+
+
+
+
+
+    addressForm = forms.AddressForm()
+    if request.method == 'POST':
+        addressForm = forms.AddressForm(request.POST)
+        if addressForm.is_valid():
+            
+            email = addressForm.cleaned_data['Email']
+            mobile=addressForm.cleaned_data['Mobile']
+            address = addressForm.cleaned_data['Address']
+            total=0
+            if 'product_ids' in request.COOKIES:
+                product_ids = request.COOKIES['product_ids']
+                if product_ids != "":
+                    product_id_in_cart=product_ids.split('|')
+                    products=models.Product.objects.all().filter(id__in = product_id_in_cart)
+                    for p in products:
+                        total=total+p.price
+
+            response = render(request, 'ecom/payment.html',{'total':total})
+            response.set_cookie('email',email)
+            response.set_cookie('mobile',mobile)
+            response.set_cookie('address',address)
+            return response
+    return render(request,'ecom/customer_address.html',{'addressForm':addressForm,'product_in_cart':product_in_cart,'product_count_in_cart':product_count_in_cart})
+
+
+
+
+
+@login_required(login_url='customerlogin')
+def payment_success_view(request):
+    
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    products=None
+    email=None
+    mobile=None
+    address=None
+    if 'product_ids' in request.COOKIES:
+        product_ids = request.COOKIES['product_ids']
+        if product_ids != "":
+            product_id_in_cart=product_ids.split('|')
+            products=models.Product.objects.all().filter(id__in = product_id_in_cart)
+            
+    if 'email' in request.COOKIES:
+        email=request.COOKIES['email']
+    if 'mobile' in request.COOKIES:
+        mobile=request.COOKIES['mobile']
+    if 'address' in request.COOKIES:
+        address=request.COOKIES['address']
+
+    
+    response = render(request,'ecom/payment_success.html')
+    response.delete_cookie('product_ids')
+    response.delete_cookie('email')
+    response.delete_cookie('mobile')
+    response.delete_cookie('address')
+    return response
+
+
+
+
+@login_required(login_url='customerlogin')
+@user_passes_test(is_customer)
+def my_order_view(request):
+    customer=models.Customer.objects.get(user_id=request.user.id)
+    orders=models.Orders.objects.all().filter(customer_id = customer)
+    ordered_products=[]
+    for order in orders:
+        ordered_product=models.Product.objects.all().filter(id=order.product.id)
+        ordered_products.append(ordered_product)
+
+    return render(request,'ecom/my_order.html',{'data':zip(ordered_products,orders)})
+
+s
+
+def contactus_view(request):
+    sub = forms.ContactusForm()
+    if request.method == 'POST':
+        sub = forms.ContactusForm(request.POST)
+        if sub.is_valid():
+            email = sub.cleaned_data['Email']
+            name=sub.cleaned_data['Name']
+            message = sub.cleaned_data['Message']
+            send_mail(str(name)+' || '+str(email),message, settings.EMAIL_HOST_USER, settings.EMAIL_RECEIVING_USER, fail_silently = False)
+            return render(request, 'ecom/contactussuccess.html')
+    return render(request, 'ecom/contactus.html', {'form':sub})
